618c38feed0bdd6178c569cc83632a9a5d0f5731
4406arthur <4406arthur@gmail.com>
2014-05-11 17:27:59 +0800

    add a admin user ArthurMa

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501101921) do
+ActiveRecord::Schema.define(version: 20140511091629) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140501101921) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
